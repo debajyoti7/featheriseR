3641716a161b6725d1ae84487af8be078f6811b4
debajyoti7 <dave0908@gmail.com>
2016-07-08 10:32:28 +0530

    list of libraries used

--- R/featheriseR_csv.R
+++ R/featheriseR_csv.R
@@ -8,20 +8,20 @@
 #' featheriseR_function()
 
 featheriseR_csv <- function(ip){
-  
+
   list.of.packages <- c("feather","magrittr")
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
   if(length(new.packages)) install.packages(new.packages)
   if(length(new.packages)) devtools::use_package(new.packages)
-  
-  library(list.of.packages, character.only = TRUE)
-  
+
+  lapply(x, require, character.only = TRUE)
+
   #if(!library(feather)){devtools::use_package("feather")}
   #if(!(library(magrittr))){devtools::use_package("magrittr")}
 
   op <- gsub(".csv", "", ip) %>%
     paste0(".feather")
-  
+
   read.csv(file=ip, header=TRUE, sep=",") %>%
     as.data.frame() %>%
     write_feather(op)
