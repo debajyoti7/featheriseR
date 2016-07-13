a9cb54d0d11882536798112782ea18fcbd5fa972
debajyoti7 <dave0908@gmail.com>
2016-07-13 11:45:12 +0530

    corrected tests

--- tests/testthat/test_featheriseR_xls.R
+++ tests/testthat/test_featheriseR_xls.R
@@ -3,9 +3,9 @@ library(xlsx)
 context("Sanctity")
 
 test_that("number of records in input and output data is same", {
-  expect_equal(dim(featheriseR_xls(system.file("examples", "applicants1.csv",
+  expect_equal(dim(featheriseR_xls(system.file("examples", "applicants2.xls",
                                            package = "featheriseR"))),
-               dim(read.xlsx(system.file("examples", "applicants1.csv",
+               dim(read.xlsx(system.file("examples", "applicants2.xls",
                                         package = "featheriseR"))))
 
 })
--- tests/testthat/test_featheriseR_xlsx.R
+++ tests/testthat/test_featheriseR_xlsx.R
@@ -3,9 +3,9 @@ library(xlsx)
 context("Sanctity")
 
 test_that("number of records in input and output data is same", {
-  expect_equal(dim(featheriseR_xlsx(system.file("examples", "applicants1.csv",
+  expect_equal(dim(featheriseR_xlsx(system.file("examples", "applicants.xlsx",
                                            package = "featheriseR"))),
-               dim(read.xlsx(system.file("examples", "applicants1.csv",
+               dim(read.xlsx(system.file("examples", "applicants.xlsx",
                                         package = "featheriseR"))))
 
 })
